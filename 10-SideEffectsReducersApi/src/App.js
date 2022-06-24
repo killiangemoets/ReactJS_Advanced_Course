3f62ae0311909eff57e1555e3e2022ffe71fb1d2
Killian Gemoets <kilgem@live.fr>
2022-06-24 05:41:31 +0200

    friday

--- 10-SideEffectsReducersApi/src/App.js
+++ 10-SideEffectsReducersApi/src/App.js
@@ -1,39 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const ctx = useContext(AuthContext);
 
-  useEffect(() => {
-    const storedUserIsLoggin = localStorage.getItem("isLoggedIn");
-
-    if (storedUserIsLoggin === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", 1);
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
+  // To have access the context and its content we need to listen and we can listen in 2 ways:
+  // - by using auth-context consumer <AuthContext.Consumer>
+  // - by using a react hook : useContext
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      <AuthContext.Provider
+        value={{
+          isLoggedIn: false,
+          onLogout: logoutHandler,
+        }}
+      >
+        <MainHeader />
+        <main>
+          {!ctx.isLoggedIn && <Login />}
+          {ctx.isLoggedIn && <Home />}
+        </main>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 }
