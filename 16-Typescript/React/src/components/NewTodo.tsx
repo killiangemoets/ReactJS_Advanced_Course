cb615692e24cceb3fa71274d238597d00ab654db
Killian Gemoets <kilgem@live.fr>
2022-07-11 11:11:36 +0200

    monday

--- 16-Typescript/React/src/components/NewTodo.tsx
+++ 16-Typescript/React/src/components/NewTodo.tsx
@@ -7,11 +7,13 @@ import classes from "./NewTodo.modules.css";
 const NewTodo: React.FC = () => {
   const todosCtx = useContext(TodosContext);
 
+  // We now have to specify which concrete value or HTML element we plan on storing into this specific ref object we are creating here
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
+    // The exclamation mark tells TypeScript that you know that this possibly nullish value will never be null in this spot
     const enteredText = todoTextInputRef.current!.value;
 
     if (enteredText.trim().length === 0) {
